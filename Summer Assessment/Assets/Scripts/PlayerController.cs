c485f36fbc23b62dc1090f082d57341f68d380bd
timzzhd <74539817+timzzhd@users.noreply.github.com>
2021-08-22 02:36:30 +0100

    coin added

--- Summer Assessment/Assets/Scripts/PlayerController.cs
+++ Summer Assessment/Assets/Scripts/PlayerController.cs
@@ -12,18 +12,26 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
 
-    private float speed = 20.0f;
-    private float turnSpeed = 45.0f;
+ 
     private float horizontalInput;
     private float forwardInput;
+    private float speed = 10.0f;
+    private float turnSpeed = 5.0f;
+
 
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+
     }
+
+
+
+
+
+
 }
+
